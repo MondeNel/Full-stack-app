0b50acf1dd6ac30700372aa3c540e36c44aa4029
mondenel <mondenel1996@gmail.com>
2026-02-26 10:10:39 +0200

    Now we need to register our service in the DI system

--- AuthBackend/Program.cs
+++ AuthBackend/Program.cs
@@ -41,6 +41,10 @@ builder.Services.AddAuthentication(options =>
         };
     });
 
+builder.Services.AddScoped<IAuthenticationService, AuthenticationService>();
+
+builder.Services.AddControllers();
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
