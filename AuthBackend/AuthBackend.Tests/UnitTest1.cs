54e3961a29e440e9f66ec679c0cce64927b28900
mondenel <mondenel1996@gmail.com>
2026-02-28 16:16:48 +0200

    Setup .NET 10 environment, fixed project references, and configured xUnit test project

---
+++ AuthBackend/AuthBackend.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+﻿using Xunit;
+
+namespace AuthenticationApi.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void SampleTest()
+        {
+            Assert.True(true);
+        }
+    }
+}
