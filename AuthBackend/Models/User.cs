61b0a7d80b82c6061b553be12f9c5b4b861a3444
mondenel <mondenel1996@gmail.com>
2026-02-28 14:39:01 +0200

    Improve User model documentation and extensibility

--- AuthBackend/Models/User.cs
+++ AuthBackend/Models/User.cs
@@ -1,13 +1,20 @@
 using Microsoft.AspNetCore.Identity;
 
-namespace AuthenticationApi.Entities
+namespace AuthenticationApi.Models
 {
     /// <summary>
-    /// Application user entity extending ASP.NET Identity
+    /// Represents an application user within the authentication system.
+    /// Extends <see cref="IdentityUser"/> to leverage ASP.NET Core Identity
+    /// for secure user management.
     /// </summary>
     public class User : IdentityUser
     {
-        // Add custom properties here if needed in the future
+        /// <summary>
+        /// Gets or sets the UTC date and time when the user was created.
+        /// </summary>
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        // Custom domain-specific fields can be added here if required.
         // Example:
         // public string FirstName { get; set; }
     }
