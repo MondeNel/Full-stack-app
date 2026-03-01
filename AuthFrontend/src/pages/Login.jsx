de0d80e1021352e1f23860f0949fb54510f1a642
mondenel <mondenel1996@gmail.com>
2026-03-01 10:33:10 +0200

    Redesign Login page using Material UI components with icons for inputs

--- AuthFrontend/src/pages/Login.jsx
+++ AuthFrontend/src/pages/Login.jsx
@@ -1,14 +1,15 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { loginUser } from "../api/auth";
-import { TextField, Button, Container, Typography, Box, Paper } from "@mui/material";
+import { TextField, Button, Container, Typography, Box, Paper, InputAdornment } from "@mui/material";
+import EmailIcon from "@mui/icons-material/Email";
+import LockIcon from "@mui/icons-material/Lock";
 
 const Login = () => {
   const [form, setForm] = useState({ email: "", password: "" });
   const navigate = useNavigate();
 
-  const handleChange = (e) =>
-    setForm({ ...form, [e.target.name]: e.target.value });
+  const handleChange = (e) => setForm({ ...form, [e.target.name]: e.target.value });
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -23,16 +24,28 @@ const Login = () => {
 
   return (
     <Container maxWidth="sm">
-      <Paper elevation={3} sx={{ padding: 4, mt: 8 }}>
-        <Typography variant="h4" align="center" gutterBottom>
-          Sign In
-        </Typography>
+      <Paper elevation={5} sx={{ padding: 4, mt: 8 }}>
+        <Typography variant="h4" align="center" gutterBottom>Sign In</Typography>
         <Box component="form" onSubmit={handleSubmit} sx={{ mt: 2, display: "flex", flexDirection: "column", gap: 2 }}>
-          <TextField label="Email" name="email" type="email" onChange={handleChange} fullWidth required />
-          <TextField label="Password" name="password" type="password" onChange={handleChange} fullWidth required />
-          <Button type="submit" variant="contained" color="primary" size="large">
-            Login
-          </Button>
+          <TextField
+            label="Email"
+            name="email"
+            type="email"
+            onChange={handleChange}
+            fullWidth
+            required
+            InputProps={{ startAdornment: <InputAdornment position="start"><EmailIcon /></InputAdornment> }}
+          />
+          <TextField
+            label="Password"
+            name="password"
+            type="password"
+            onChange={handleChange}
+            fullWidth
+            required
+            InputProps={{ startAdornment: <InputAdornment position="start"><LockIcon /></InputAdornment> }}
+          />
+          <Button type="submit" variant="contained" color="primary" size="large">Login</Button>
         </Box>
       </Paper>
     </Container>
