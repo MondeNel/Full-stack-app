a7a6b0f0b25a440e5c3b90e1d4706fff7ba4720e
mondenel <mondenel1996@gmail.com>
2026-03-01 10:38:09 +0200

    Add API functions for register, login, and getUserDetails using Axios

--- AuthFrontend/src/api/auth.js
+++ AuthFrontend/src/api/auth.js
@@ -3,27 +3,27 @@ import axios from "axios";
 const API_URL = "http://localhost:5277"; // Connecting to backend URL
 
 /**
- * @param {Object} userData
- * @returns Promise
+ * Register a new user
+ * @param {Object} data - { firstName, lastName, email, password }
  */
-export const registerUser = (userData) => {
-  return axios.post(`${API_URL}/user/register`, userData);
+export const registerUser = (data) => {
+  return axios.post(`${API_URL}/register`, data);
 };
 
 /**
- * @param {Object} credentials
- * @returns Promise
+ * Login a user
+ * @param {Object} data - { email, password }
  */
-export const loginUser = (credentials) => {
-  return axios.post(`${API_URL}/user/login`, credentials);
+export const loginUser = (data) => {
+  return axios.post(`${API_URL}/login`, data);
 };
 
 /**
- * @param {string} token
- * @returns Promise
+ * Get current user details (requires token)
+ * @param {string} token - JWT token
  */
 export const getUserDetails = (token) => {
-  return axios.get(`${API_URL}/user/details`, {
+  return axios.get(`${API_URL}/user`, {
     headers: { Authorization: `Bearer ${token}` },
   });
 };
